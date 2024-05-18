1de948a9387b276a9aa9fe8000e7e3d108ef217e
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-05-19 05:04:01 +0900

    [feat] setNickName 뷰

--- app/src/main/java/com/sopt/now/sopkathon/android/ui/friendList/FriendListFragment.kt
+++ app/src/main/java/com/sopt/now/sopkathon/android/ui/friendList/FriendListFragment.kt
@@ -6,5 +6,5 @@ import com.sopt.now.sopkathon.android.databinding.FragmentSetNicknameBinding
 import com.sopt.now.sopkathon.android.ui.common.base.BindingFragment
 
 class FriendListFragment :
-    BindingFragment<FragmentSetNicknameBinding>(R.layout.fragment_set_nickname) {
+    BindingFragment<FragmentFriendListBinding>(R.layout.fragment_friend_list) {
 }
--- app/src/main/java/com/sopt/now/sopkathon/android/ui/setNickName/SetNicknameFragment.kt
+++ app/src/main/java/com/sopt/now/sopkathon/android/ui/setNickName/SetNicknameFragment.kt
@@ -1,8 +1,47 @@
 package com.sopt.now.sopkathon.android.ui.setNickName
 
+import android.os.Bundle
+import android.view.View
+import androidx.compose.ui.graphics.Color
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.sopt.now.sopkathon.android.R
-import com.sopt.now.sopkathon.android.databinding.FragmentFriendListBinding
+import com.sopt.now.sopkathon.android.databinding.FragmentSetNicknameBinding
 import com.sopt.now.sopkathon.android.ui.common.base.BindingFragment
 
-class SetNickNameFragment: BindingFragment<FragmentFriendListBinding>(R.layout.fragment_friend_list){
+class SetNickNameFragment :
+    BindingFragment<FragmentSetNicknameBinding>(R.layout.fragment_set_nickname) {
+    private val viewmodel by viewModels<SetNicknameViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeNickNameEditText()
+        observeInputNickName()
+        initBtnClickListener()
+
+    }
+
+    private fun observeNickNameEditText(){
+        binding.etSetNicknameInputNickname.doAfterTextChanged {
+            viewmodel.updateNickName(it.toString())
+        }
+    }
+
+    private fun observeInputNickName(){
+        viewmodel.nickName.observe(viewLifecycleOwner){
+            if(viewmodel.checkInvalidNickName()==false){
+                binding.viewSetNicknameInputText.setBackgroundColor(0xFFFFFF)
+            }
+
+        }
+    }
+    private fun initBtnClickListener() {
+        binding.btnAsdas.setOnClickListener {
+
+
+
+            findNavController().navigate(R.id.fragment_friend_list)
+        }
+    }
 }
