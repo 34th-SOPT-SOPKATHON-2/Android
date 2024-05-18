b934b61e3c271e73f5045b40478d74036672323e
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-05-19 05:08:12 +0900

    [feat] 닉네임 수정 뷰 수정

--- app/src/main/java/com/sopt/now/sopkathon/android/ui/setNickname/SetNicknameFragment.kt
+++ app/src/main/java/com/sopt/now/sopkathon/android/ui/setNickname/SetNicknameFragment.kt
@@ -1,8 +1,37 @@
-package com.sopt.now.sopkathon.android.ui.setNickname
+package com.sopt.now.sopkathon.android.ui.setNickName
 
+import android.os.Bundle
+import android.view.View
+import androidx.compose.ui.graphics.Color
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.sopt.now.sopkathon.android.R
 import com.sopt.now.sopkathon.android.databinding.FragmentSetNicknameBinding
 import com.sopt.now.sopkathon.android.ui.common.base.BindingFragment
 
-class SetNicknameFragment: BindingFragment<FragmentSetNicknameBinding>(R.layout.fragment_set_nickname) {
+class SetNickNameFragment :
+    BindingFragment<FragmentSetNicknameBinding>(R.layout.fragment_set_nickname) {
+    private val viewmodel by viewModels<SetNicknameViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeNickNameEditText()
+        observeInputNickName()
+    }
+
+    private fun observeNickNameEditText(){
+        binding.etSetNicknameInputNickname.doAfterTextChanged {
+            viewmodel.updateNickName(it.toString())
+        }
+    }
+
+    private fun observeInputNickName(){
+        viewmodel.nickName.observe(viewLifecycleOwner){
+            if(viewmodel.checkInvalidNickName()==false){
+                binding.viewSetNicknameInputText.setBackgroundColor(0xFFFFFF)
+            }
+
+        }
+    }
 }
